2368bd917a24d450c1c2e8b3fe70435bdfba53b7
rgould <rwgould@gmail.com>
2013-05-06 12:43:29 -0700

    Positions now report adjacent positions

--- spec/lib/position.spec.js
+++ spec/lib/position.spec.js
@@ -1,5 +1,18 @@
 define(['lib/position'], function(position) {
 describe("Position", function() {
+  beforeEach(function() {
+    this.addMatchers({
+      toBePosition: function(expected) {
+        var actual = this.actual.asKey();
+        var expected = expected.asKey();
+        var notText = this.isNot ? " not" : "";
+        this.message = function() {
+          return "Expected " + actual + notText + " to be " + expected;
+        };
+        return actual === expected;
+      }
+    });
+  });
 
   describe("creation", function() {
     it("should set circle and rank", function() {
@@ -64,6 +77,61 @@ describe("Position", function() {
     it("should convert to a key suitable for indexes", function() {
       var pos = position(2)(3);
       expect(pos.asKey()).toBe("2,3");
+    });
+  });
+
+  describe("reporting adjacent positions", function() {
+    it("should report adjacent positions on the outer circle", function() {
+      var positions = position(0)(0).adjacentPositions();
+      expect(positions.length).toBe(4);
+      expect(positions[0]).toBePosition(position(0)(1));
+      expect(positions[1]).toBePosition(position(0)(11));
+      expect(positions[2]).toBePosition(position(1)(0));
+      expect(positions[3]).toBePosition(position(1)(1));
+    });
+    it("should report adjacent positions on middle circles", function() {
+      var positions = position(1)(0).adjacentPositions();
+      expect(positions.length).toBe(6);
+      expect(positions[0]).toBePosition(position(0)(0));
+      expect(positions[1]).toBePosition(position(0)(11));
+      expect(positions[2]).toBePosition(position(1)(1));
+      expect(positions[3]).toBePosition(position(1)(11));
+      expect(positions[4]).toBePosition(position(2)(0));
+      expect(positions[5]).toBePosition(position(2)(1));
+    });
+    it("should report adjacent positions on the inner circle", function() {
+      var positions = position(3)(0).adjacentPositions();
+      expect(positions.length).toBe(5);
+      expect(positions[0]).toBePosition(position(2)(0));
+      expect(positions[1]).toBePosition(position(2)(11));
+      expect(positions[2]).toBePosition(position(3)(1));
+      expect(positions[3]).toBePosition(position(3)(11));
+      expect(positions[4]).toBePosition(position(4)(0));
+    });
+    it("should report adjacent positions from the capital", function() {
+      var positions = position(4)(0).adjacentPositions();
+      expect(positions.length).toBe(12);
+      for (var i = 0; i < 12; i++) {
+        expect(positions[i]).toBePosition(position(3)(i));
+      }
+    });
+  });
+
+  describe("incrementing rank", function() {
+    it("should increment normally", function() {
+      expect(position.incrementRank(1)).toBe(2);
+    });
+    it("should cycle past the boundary", function() {
+      expect(position.incrementRank(11)).toBe(0);
+    });
+  });
+
+  describe("decrementing rank", function() {
+    it("should decrement normally", function() {
+      expect(position.decrementRank(11)).toBe(10);
+    });
+    it("should cycle past the boundary", function() {
+      expect(position.decrementRank(0)).toBe(11);
     });
   });
 });
